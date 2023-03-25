bbf616fd24a793843f89adeb78c144a0b1f866bc
Yusen Wu <sanmusen_wu@outlook.com>
2023-03-25 19:03:42 +0800

    Feat: recover search

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { message, Input, Button } from 'antd';
 import { reqInput } from '../api';
+import useLocalStorage from '../hooks/useLocalStorage';
 
-export const getJsonToTree=(data) => {
+export const getJsonToTree = (data) => {
   // 此处返回json结构
   // 在DirectoryTree使用时加【】
-  let obj={
+  let obj = {
     title: data.name,
     key: data.rel_path
   }
   // console.log(data, 'data');
 
   if (data.type === "folder") {
-    obj['children']=[]
+    obj['children'] = []
     data.folders.forEach(i => {
       obj.children.push(getJsonToTree(i))
     })
@@ -20,40 +21,30 @@ export const getJsonToTree=(data) => {
       obj.children.push(getJsonToTree(i))
     })
   }
-  if(obj.children&&obj.children.length===0){
+  if (obj.children && obj.children.length === 0) {
     delete obj.children
   }
 
   return obj
 }
 
-class InputDemo extends React.Component {
-  state = {
-    InputValue: "git@github.com:zhyAmber/FYP.git",//输入框默认输入值
-    content: '',
-  };
+const InputDemo = (props) => {
+  const [InputValue, setInputValue] = useLocalStorage("clonedir", "git@github.com:zhyAmber/FYP.git")
+  const [content, setContent] = useState("")
 
-  componentDidMount() {
-
-
-  }
-
-  handleGetInputValue = (event) => {
-    this.setState({
-      InputValue: event.target.value,
-    });
+  const handleGetInputValue = (event) => {
+    setInputValue(event.target.value)
     //let Storagedata=JSON.parse(this.localStorage.getItem('result.data: '))
     //  console.log("Storagedata",Storagedata)
     // this.props.getTreeData(Storagedata.reponame,this.getData([Storagedata.foldertree]))
   };
 
 
-  getData = (data)=>{
+  const getData = (data) => {
     return getJsonToTree(data)
   }
 
-  InputSubmit = async () => {
-    const { InputValue } = this.state;
+  const InputSubmit = async () => {
     console.log('传给后端的输入框数据value: ', InputValue);
     message.destroy()
     message.loading("Send Clone Request")
@@ -67,17 +58,14 @@ class InputDemo extends React.Component {
       message.destroy()
       message.success("Sucessful Clone")
       localStorage.setItem('result.data: ', JSON.stringify(result.data))
-      const foldertreejson=this.getData(result.data.foldertree)
-      console.log("文档树结构2",foldertreejson)
-      this.props.getTreeData(result.data.reponame, foldertreejson)
+      const foldertreejson = getData(result.data.foldertree)
+      console.log("文档树结构2", foldertreejson)
+      props.getTreeData(result.data.reponame, foldertreejson)
 
       // localStorage.setItem('result.data: ', JSON.stringify(result.data))
       //this.props.getTreeData(Storagedata.reponame,this.getData(Storagedata.foldertree))
-      this.setState({
-        content: result.data.foldertree.files,
-        localStorage: result,
-      })
-      this.props.setCommitHis(result.data.logs)
+      setContent(result.data.foldertree.files)
+      props.setCommitHis(result.data.logs)
     } else {
       message.destroy()
       message.error('Error when clone');
@@ -85,18 +73,18 @@ class InputDemo extends React.Component {
     //console.log('localstorage',localStorage)
   };
 
-  render() {
-    return (
-      <div >
-        Target Repository:
-        <Input
-          style={{ width: "50%" }}
-          value={this.state.InputValue}
-          onChange={this.handleGetInputValue}
-        />
-        <Button type="primary" onClick={this.InputSubmit}>Clone</Button>
 
-        {/* <div>
+  return (
+    <div >
+      Target Repository:
+      <Input
+        style={{ width: "50%" }}
+        value={InputValue}
+        onChange={handleGetInputValue}
+      />
+      <Button type="primary" onClick={InputSubmit}>Clone</Button>
+
+      {/* <div>
           <input
           value={this.state.content}
           onChange={this.handleGetInputValue}
@@ -104,9 +92,9 @@ class InputDemo extends React.Component {
         </div> */}
 
 
-      </div>
-    )
-  }
+    </div>
+  )
+
 }
 
 export default InputDemo;
